240ce6be08cac677e7cfa6a9b94dd584aa02292b
Roshin Nishad <roshin006@gmail.com>
2025-08-06 23:40:14 +0530

    Added FastAPI LangChain connection

--- main.py
+++ main.py
@@ -13,9 +13,19 @@ from langchain.chat_models import init_chat_model
 
 from dotenv import load_dotenv
 
+import requests
+
 load_dotenv()  # loads OPENAI_API_KEY
 
 llm = init_chat_model("openai:gpt-4.1-mini")
+
+
+class KeyDetails(TypedDict):
+    tracking_id: str
+    origin: str
+    destination: str
+    pickup_date: str
+    weight: str
 
 
 class Email(TypedDict):
@@ -24,7 +34,8 @@ class Email(TypedDict):
     body: str
     customer_name: str
     email_type: str
-    key_details: List[str]
+    key_details: KeyDetails
+    api_response: dict
 
 
 class State(TypedDict, total=False):
@@ -32,6 +43,7 @@ class State(TypedDict, total=False):
     raw_emails: str
     formatted_emails: List[Email]
     parsed_and_categorized_emails: List[Email]
+    api_response_emails: List[Email]
 
 
 def extract_emails(state: State) -> State:
@@ -74,28 +86,68 @@ def parse_and_categorize_emails(state: State) -> State:
     parsed_and_categorized_emails = []
     formatted_emails = state["formatted_emails"]
     for email in formatted_emails:
+        key_details = KeyDetails()
         prompt = (
             "The email is: {email}\n"
-            "I want you to reply 'tracking' if it is a tracking email or 'booking' if it is a booking email. Then in a new line I want you extract the following information if available (tracking number, booking ID, locations like x to y, item type like fragile etc., dates etc) and write it as comma seperated values (no spaces) with a label attached like 'tracking number: ---,date: July 7' etc."
+            "I want you to reply 'tracking' if it is a tracking email or 'booking' if it is a booking email. Then in a new line I want you extract the following information if available in this order (tracking number, origin, destination, pickup date, weight) and write it as comma seperated values (no spaces). If a specific value isn't available, just leave it blank like (,Mumbai,Bangalore,July 7,40)."
         ).format(email=str(email))
         result = llm.invoke(prompt)
         email_info = result.content.split("\n")
         email["email_type"] = email_info[0]
-        email["key_details"] = email_info[1].split(",")
+        raw_key_details = email_info[1].split(",")
+        key_details["tracking_id"] = raw_key_details[0]
+        key_details["origin"] = raw_key_details[1]
+        key_details["destination"] = raw_key_details[2]
+        key_details["pickup_date"] = raw_key_details[3]
+        key_details["weight"] = raw_key_details[4]
+        email["key_details"] = key_details
         parsed_and_categorized_emails.append(email)
     return {"parsed_and_categorized_emails": parsed_and_categorized_emails}
+
+
+def call_api(state: State) -> State:
+    api_response_emails = []
+    parsed_and_categorized_emails = state["parsed_and_categorized_emails"]
+    for email in parsed_and_categorized_emails:
+        key_details = email["key_details"]
+        if email["email_type"] == "booking":
+            payload = {
+                "origin": key_details["origin"],
+                "destination": key_details["destination"],
+                "pickup_date": key_details["pickup_date"],
+                "weight": key_details["weight"],
+            }
+            resp = requests.post("http://127.0.0.1:8000/book_shipment", json=payload)
+            data = resp.json()
+            email["api_response"] = data
+
+        if email["email_type"] == "tracking":
+            payload = {"tracking_id": key_details["tracking_id"]}
+            resp = requests.post("http://127.0.0.1:8000/track_shipment", json=payload)
+            data = resp.json()
+            email["api_response"] = data
+        api_response_emails.append(email)
+    return {"api_response_emails": api_response_emails}
 
 
 graph = StateGraph(State)
 graph.add_node("extract_emails", extract_emails)
 graph.add_node("format_emails", format_emails)
 graph.add_node("parse_and_categorize_emails", parse_and_categorize_emails)
+graph.add_node("call_api", call_api)
 
 graph.add_edge(START, "extract_emails")
 graph.add_edge("extract_emails", "format_emails")
 graph.add_edge("format_emails", "parse_and_categorize_emails")
-graph.add_edge("parse_and_categorize_emails", END)
+graph.add_edge("parse_and_categorize_emails", "call_api")
+graph.add_edge("call_api", END)
 
 app = graph.compile()
 final_state = app.invoke({"data_file": "testdata.txt"})
-print([email["key_details"] for email in final_state["parsed_and_categorized_emails"]])
+# print(
+#     [
+#         [email["email_type"], email["api_response"]]
+#         for email in final_state["api_response_emails"]
+#     ]
+# )
+print(final_state["api_response_emails"])
--- shipping_api.py
+++ shipping_api.py
@@ -4,23 +4,45 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from datetime import date
+import random
+
+app = FastAPI()
+
 
 class BookingRequest(BaseModel):
     origin: str
     destination: str
-    pickup_date: date
-    weight_kg: float
-    
+    pickup_date: str
+    weight: str
+
+
 class TrackingRequest(BaseModel):
     tracking_id: str
-    
+
+
 class BookingResponse(BaseModel):
     confirmation_id: str
     eta_days: int
-    
+
+
 class TrackingResponse(BaseModel):
     status: str
     last_seen: str
-    
-# >* PICK RANDOM OPTION FROM LIST IN TRANSIT LOST MUMBAI DELHI ETC
+
+
+@app.post("/book_shipment", response_model=BookingResponse)
+def book_shipment(req: BookingRequest):
+    return BookingResponse(
+        confirmation_id=f"BK{random.randint(10000, 99999)}",
+        eta_days=random.randint(1, 50),
+    )
+
+
+@app.post("/track_shipment", response_model=TrackingResponse)
+def track_shipment(req: TrackingRequest):
+    return TrackingResponse(
+        status=random.choice(
+            ["IN TRANSIT", "LOST", "DELAYED", "SHIPPED", "OUT FOR DELIVERY"]
+        ),
+        last_seen=f"{random.choice(["Mumbai", "Delhi", "Bangalore", "Noida", "Goa"])} warehouse",
+    )
